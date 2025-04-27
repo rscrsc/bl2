3d5276c83df38798650c8e7979268e64579dd30f
Arcie Ren <r2277471064@163.com>
2025-04-27 10:53:50 +0800

    apply ip config in cli (not tested yet)

--- main.cpp
+++ main.cpp
@@ -19,32 +19,39 @@
 #include "utils.h"
 
 int main(int argc, char** argv) {
+
 std::thread eh(utils::eventHandler);
 
 args::ArgumentParser parser("Default prompt");
 args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
 args::ValueFlag<double> timeIntv(parser, "timeIntv", "Time interval (in sec)", {'t'});
 args::PositionalList<std::string> ips(parser, "ips", "Client IP list");
+
 try{
     parser.ParseCLI(argc, argv);
-    std::cout << "Time: " << args::get(timeIntv) << std::endl;
-    timespec TIMEOUT { .tv_sec=0, .tv_nsec=1'000'000 };
-    for (auto& ip : args::get(ips)) {
-        std::cout << ip << std::endl;
+    int timeIntvSec = static_cast<int>(args::get(timeIntv));
+    int timeIntvNsec = static_cast<int>((args::get(timeIntv) - timeIntvSec)*1'000'000'000);
+    timespec TIMEOUT { .tv_sec=timeIntvSec, .tv_nsec=timeIntvNsec };
+    std::vector<std::string>& ipStrs = args::get(ips);
+
+    std::vector<NFQueue> nfqs;
+    nfqs.reserve(ipStrs.size());
+    for (int i = 0; i < ipStrs.size(); ++i) {
+        nfqs.emplace_back(i, &utils::onPacket);
     }
-    NFQueue nfqA(0, &utils::onPacket);
-    NFQueue nfqB(1, &utils::onPacket);
+
     utils::UniqueFd epollUFd(epoll_create1(NOFLAG));
     int epollFd = epollUFd.fd;
     if (epollFd == -1) {
         throw std::runtime_error("unable to create epoll fd");
     }
-    if (epoll_ctl(epollFd, EPOLL_CTL_ADD, nfqA.ev.data.fd, &nfqA.ev) == -1) {
-        throw std::runtime_error("unable to add an event to epoll");
+
+    for (int i = 0; i < ipCnt; ++i) {
+        if (epoll_ctl(epollFd, EPOLL_CTL_ADD, nfqs[i].ev.data.fd, &(nfqs[i].ev)) == -1) {
+            throw std::runtime_error("unable to add an event to epoll");
+        }
     }
-    if (epoll_ctl(epollFd, EPOLL_CTL_ADD, nfqB.ev.data.fd, &nfqB.ev) == -1) {
-        throw std::runtime_error("unable to add an event to epoll");
-    }
+
     utils::UniqueFd timerUFd(timerfd_create(CLOCK_MONOTONIC, 0));
     int timerFd = timerUFd.fd;
     if (timerFd == -1) {
@@ -61,43 +68,44 @@ try{
             throw std::runtime_error("unable to add an event to epoll");
         }
     }
-    Faker udpA("192.168.69.139", 12345);
-    Faker udpB("192.168.69.67", 12345);
-    int queueNow = 1;
+
+    std::vector<Faker> fakers{};
+    fakers.reserve(ipStrs.size());
+    for (auto& ipStr : ipStrs) {
+        fakers.emplace_back(ipStr, 12345);
+    }
+
+    int queueNow = nfqs.size() - 1;
     while (utils::running) {
-        std::array<epoll_event, 3> evBuffer;
+
+        std::vector<epoll_event> evBuffer;
+        evBuffer.resize(ipStrs.size() + 1, {});
         int rPoll = epoll_wait(epollFd, evBuffer.data(), evBuffer.size(), -1); 
         if (rPoll < 0) {
             throw std::runtime_error("failed waiting epoll");
         }
-        bool hasA = false, hasB = false;
+
+        std::vector<bool> isRecv;
+        isRecv.resize(nfqs.size(), {});
         for (int i = 0; i < rPoll; ++i) {
-            if (evBuffer[i].data.fd == nfqA.sockFd) {
-                nfqA.process();
-                hasA = true;
-            }
-            if (evBuffer[i].data.fd == nfqB.sockFd) {
-                nfqB.process();
-                hasB = true;
+            for (int j = 0; j < nfqs.size(); ++j) {
+                if (evBuffer[i].data.fd == nfq[i].sockFd) {
+                    nfqs[i].process();
+                    isRecv[i] = true;
+                }
             }
             if (evBuffer[i].data.fd == timerFd) {
-                queueNow = (queueNow + 1) % 2;
+                queueNow = (queueNow + 1) % nfqs.size();
 
                 uint64_t expirations;
                 read(timerFd, &expirations, sizeof(expirations));
                 timerfd_settime(timerFd, NOFLAG, &timerSpec, nullptr);
-                if (queueNow == 0 && (!hasA)) {
-                    udpA.send();
+                if (!isRecv[queueNow]) {
+                    fakers[queueNow].send();
                     //std::cout << "\033[2J\033[H";
                     //dumpTime(std::cout);
                     //std::cout << "  ";
                     //std::cout << "Sent complement UDP to A" << std::endl;
-                } else if (queueNow == 1 && (!hasB)) {
-                    udpB.send();
-                    //std::cout << "\033[2J\033[H";
-                    //dumpTime(std::cout);
-                    //std::cout << "  ";
-                    //std::cout << "Sent complement UDP to B" << std::endl;
                 }
             }
         }
